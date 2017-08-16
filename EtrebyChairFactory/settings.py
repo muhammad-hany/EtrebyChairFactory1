78440ee2d2ec34874a4c4b7a23bc4ba80a3b6494
muhammad-hany <mohammedeletreby@gmail.com>
2017-08-16 22:41:36 +0200

    before deployment

--- EtrebyChairFactory/settings.py
+++ EtrebyChairFactory/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '10%h&m45)^@4r4r-o#6#$1h48*e6lg5+oz+e=r&zuk@p600gv$'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -131,6 +131,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-SECRET_KEY=os.environ.get('SECRET_KEY','10%h&m45)^@4r4r-o#6#$1h48*e6lg5+oz+e=r&zuk@p600gv$')
+SECRET_KEY=os.environ['SECRET_KEY']
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 
