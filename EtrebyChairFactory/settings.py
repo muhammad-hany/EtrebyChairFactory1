e36461f43d1bdbb8f0e47bab56a47ef1a31f5d68
muhammad-hany <mohammedeletreby@gmail.com>
2017-08-18 20:52:43 +0200

    before deployment 18.8.2017 8.52

--- EtrebyChairFactory/settings.py
+++ EtrebyChairFactory/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+PROJECT_ROOT=os.path.dirname(os.path.abspath(__file__))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
