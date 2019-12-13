42e7cf5e6351160a97d2a22ba4765ece1316c400
Muhammad <mohammedeletreby@gmail.com>
2019-12-13 09:13:23 +0200

    added descrption to details

--- Site/admin.py
+++ Site/admin.py
@@ -1,25 +1,21 @@
 from django.contrib import admin
-from Site.models import Chair,ChairImages
+from Site.models import Chair, ChairImages
 
 
 class ChairImageInline(admin.StackedInline):
     model = ChairImages
     extra = 0
-    fields = ['photo_tag','image']
-    readonly_fields = ['photo_tag',]
-
+    fields = ['photo_tag', 'image']
+    readonly_fields = ['photo_tag', ]
 
 
 class ChairAdmin(admin.ModelAdmin):
     readonly_fields = ['date']
-    inlines = [ChairImageInline,]
+    inlines = [ChairImageInline, ]
 
-    list_display = ['name','id']
-
-
+    list_display = ['name', 'id']
 
 
-admin.site.register(Chair,ChairAdmin)
-
+admin.site.register(Chair, ChairAdmin)
 
 # Register your models here.
