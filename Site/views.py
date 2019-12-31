650d1f6b2265c3229e9032c52e6e1412253c59fa
mohammed <mohammedeletreby@gmail.com>
2019-12-31 17:23:57 +0200

    added images slide show

--- Site/views.py
+++ Site/views.py
@@ -1,6 +1,6 @@
-
 from django.shortcuts import render
 from .models import Chair
+
 
 # Create your views here.
 def home(request):
@@ -10,20 +10,21 @@ def home(request):
         thmub_nails.append(chair.chairimages_set.first())
 
     context = {'chairs': chairs, 'thumb_nails': thmub_nails}
-    return render(request, 'home.html',context)
+    return render(request, 'home.html', context)
+
 
 def index(request):
-
-    thmub_nails=[]
-    chairs=Chair.objects.all()
+    thmub_nails = []
+    chairs = Chair.objects.all()
     for chair in chairs:
         thmub_nails.append(chair.chairimages_set.first())
 
-    context={'chairs':chairs,'thumb_nails':thmub_nails}
-    return render(request,'index.html',context)
+    context = {'chairs': chairs, 'thumb_nails': thmub_nails}
+    return render(request, 'index.html', context)
+
 
 def details(request, id):
-    chair=Chair.objects.get(pk=id)
-    images=chair.chairimages_set.all()
-    context={'chair':chair,'images':images}
+    chair = Chair.objects.get(pk=id)
+    images = chair.chairimages_set.all()
+    context = {'chair': chair, 'images': images}
     return render(request, 'details.html', context)
